945214d5032ad03521de1f407e368f6cade141ab
nero <yeomfilm@gmail.com>
2023-01-26 19:33:20 +0900

    React

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,31 +3,38 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [sub_title, changeTitle] = useState([
+  let [sub_title, setTitle] = useState([
     "남자 코트 추천",
     "강남 우동맛집",
     "파이썬독학",
   ]);
-  console.log(sub_title);
-  let [likeBtn, likeCount] = useState(0);
+  let [likeBtn, setCount] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+
   let onClick = () => {
-    likeCount(likeBtn + 1);
+    for (let i = 0; i < 3; i++) {
+      if (likeBtn[i]) {
+        setCount(likeBtn[i] + 1);
+      }
+    }
   };
+
   let onClick2 = () => {
     let copy = [...sub_title];
     copy[0] = "여자코트 추천";
-    changeTitle(copy);
+    setTitle(copy);
   };
   function sort() {
     let copy = [...sub_title];
-    changeTitle(copy.sort());
+    setTitle(copy.sort());
   }
+
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <div className="list">
+      {/* <div className="list">
         <h4>
           {sub_title[0]} <span onClick={onClick}>⭐️</span> {likeBtn}
           <button onClick={onClick2}>change!</button>
@@ -36,15 +43,40 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{sub_title[1]}</h4>
+        <h4
+          onClick={() => {
+            setModal(!modal);
+            // setModal(modal === true ? false : true);
+          }}
+        >
+          {sub_title[1]}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
         <h4>{sub_title[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
 
-      <Modal></Modal>
+      {sub_title.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                setModal(!modal);
+              }}
+            >
+              {sub_title[i]} <span onClick={onClick}>⭐️</span> {likeBtn[i]}
+              <button onClick={onClick2}>change!</button>
+              <button onClick={sort}>sort</button>
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+
+      <Emoji />
+      {modal === true ? <Modal></Modal> : null}
     </div>
   );
 }
@@ -59,4 +91,7 @@ function Modal() {
   );
 } //component
 
+function Emoji() {
+  return <span>🎐</span>;
+}
 export default App;
