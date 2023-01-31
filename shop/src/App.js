0347e617f5c65dc609d8b0b9471fa631c2303229
nero <yeomfilm@gmail.com>
2023-01-31 19:50:38 +0900

    shop

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Container, Navbar, Nav, Row, Col } from "react-bootstrap";
-import clover from "./img/clover.jpeg";
+// import clover from "/clover.jpeg";
 import data from "./data.js";
 import { useState } from "react";
 
 function App() {
   let [clovers] = useState(data);
+  let [value] = useState([0, 1, 2]);
+  console.log(clovers);
   return (
     <div className="App">
       <Navbar bg="" variant="black" className="nav-var">
@@ -24,26 +26,23 @@ function App() {
 
       <div>
         <Row>
-          <Col sm>
-            <img src={clover} width="80%" />
-            <h4>{clovers[0].title}</h4>
-            <p>{clovers[0].content}</p>
-            <p>{clovers[0].price}</p>
-          </Col>
-          <Col sm>
-            <img src={clover} width="80%" />
-            <h4>clover</h4>
-            <p>클로버 물방울</p>
-          </Col>
-          <Col sm>
-            <img src={clover} width="80%" />
-            <h4>clover</h4>
-            <p>클로버 물방울</p>
-          </Col>
+          {clovers.map((a, i) => {
+            return <Item i={i} value={value[i]} clovers={clovers} />;
+          })}
         </Row>
       </div>
     </div>
   );
 }
 
+function Item(props) {
+  return (
+    <Col sm>
+      <img src={"img/img" + props.i + ".jpeg"} width="80%" />
+      <h4>{props.clovers[props.value].title}</h4>
+      <p>{props.clovers[props.value].content}</p>
+      <p>{props.clovers[props.value].price}</p>
+    </Col>
+  );
+}
 export default App;
--- shop/src/data.js
+++ shop/src/data.js
@@ -1,21 +1,21 @@
 let data = [
   {
     id: 0,
-    title: "clover1",
-    content: "clover water drop",
-    price: 2000,
+    title: "parfait",
+    content: "it's a cup of cherry soda parfait.",
+    price: 5000,
   },
   {
     id: 1,
-    title: "clover1",
-    content: "clover water drop",
-    price: 2000,
+    title: "cloud doll",
+    content: "it's fluffy.",
+    price: 3000,
   },
   {
     id: 2,
-    title: "clover1",
-    content: "clover water drop",
-    price: 2000,
+    title: "pink clip",
+    content: "it's a baby pink clip.",
+    price: 4000,
   },
 ];
 
